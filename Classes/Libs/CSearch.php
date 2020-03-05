9172749a6ef1f958035364150d81c593c3520435
dwizzel-dev <dwizzel.dev@gmail.com>
2020-03-04 22:53:58 -0500

    implement php search basic

--- Classes/Libs/CSearch.php
+++ Classes/Libs/CSearch.php
@@ -25,36 +25,63 @@ class CSearch{
             'data' => [
                 'cword' => $this->word,
                 'result' => [
-                    "make" => $this->searchMake(),
                     "kw" => $this->searchKw(),
+                    "make" => $this->searchMake()
                 ]    
             ]        
         ];
     }
 
     public function searchKw(){
-        $path = DIR_CACHE.'js/db-kw.en_US.data';
-        if(!file_exists($path)){
-            return [];
-        }
-        $file = fopen($path, "r");
-        $content = fgets($file);
-        $count = 0;
-        return array_map(function($item) use (&$count){
-            return [
-                'id' => $count++,
-                'name' => $item
-            ];
-        }, explode('|', $content));
+        $content = $this->openDB('kw');
+        return $this->match($content);
     }
 
     public function searchMake(){
-        $path = DIR_CACHE.'js/db-make.en_US.data';
+        $content = $this->openDB('make');
+        return $this->match($content);
+    }
+
+    private function openDB($name){
+        $path = DIR_CACHE."js/db-{$name}.en_US.data";
         if(!file_exists($path)){
             return [];
         }
         $file = fopen($path, "r");
         $content = fgets($file);
+        return $content;
+    }
+
+    private function match($content){
+        $rtn = [];
+        $matches = [];
+        $splitWord = explode(' ', trim(str_replace('/\s+/', ' ', $this->word)));
+        foreach($splitWord as $word){
+            $regex = '/\|+((?:[\d\s\w\-&\.]*\s|)'.$word.'[\d\s\w\-&\.]*)/i';
+            if(preg_match_all($regex, $content, $match) !== false){
+                $matches[] = $match[1];         
+            }
+        }
+        if(!count($matches)){
+            return $rtn;
+        }
+        $uMatch = $matches[0];
+        for($i=1;$i<count($matches);$i++){
+            $uMatch = array_intersect($uMatch, $matches[$i]);        
+        }
+        foreach($uMatch as $id=>$w){
+            if($id > 20){
+                break;
+            }
+            array_push($rtn, [
+                'id' => $id,
+                'name' => $w
+            ]);
+        }
+        return $rtn;
+    }
+
+    private function fake($content){
         $count = 0;
         return array_map(function($item) use (&$count){
             return [
